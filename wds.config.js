9192a7093aa3ae0f103dbb38d0bc3f642eec2ab7
Fernando Javier Ramirez Obando <fernandojavier.ramirez.obando.contractor@bbva.com>
2025-03-01 19:08:48 +0100

    feature: fixed when you refesh your page stays where you are

---
+++ wds.config.js
@@ -0,0 +1,6 @@
+export default {
+    appIndex: 'index.html',
+    nodeResolve: true,
+    watch: true,
+    historyApiFallback: true, // Redirige a index.html para manejar rutas en el frontend
+  };
